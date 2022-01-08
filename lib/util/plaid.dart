ab8da94b6058ad24531292a51b05a429bb1d2c29
Matt <gokloh@gmail.com>
2022-01-08 13:59:43 -0800

    created initialize link method

--- lib/util/plaid.dart
+++ lib/util/plaid.dart
@@ -1,1 +1,15 @@
-class Plaid {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Plaid {
+  static void initializeLink({
+    required String uid,
+  }) async {
+    Uri uri = Uri.parse(
+        'https://us-central1-better-finance-337412.cloudfunctions.net/plaid-create-link-token');
+    http.Response response = await http.post(uri, body: {'uid': uid});
+    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    print(decodedResponse);
+  }
+}
